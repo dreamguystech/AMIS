14a3bbe8863b8da876deccecf82edb5ffde9d2bc
DreamGuy's <dreamguystech@gmail.com>
2016-10-18 09:53:28 +0530

    updated

--- js/login.js
+++ js/login.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
 	var email    = $("#email");
 	var password = $("#password");
 	
+	androidToken = window.localStorage.getItem("androidToken");
+    iosToken = window.localStorage.getItem("iosToken");
+	_notify();
 	email.blur(validateEmailUsername);	
 	password.blur(validateLpassword);
 	email.keyup(validateEmailUsername);
@@ -58,43 +61,9 @@ $(document).ready(function(){
                     profImg_url = window.localStorage.getItem("prof_img");
                     latitude = window.localStorage.getItem("latitude");
                     longitude = window.localStorage.getItem("longitude");
-                    /*var ul_obj = data.det.ula;
-                    
-                    var usr_l = Object.keys(ul_obj).map(function(key){return ul_obj[key]});
-
-                    for (var xy = 0; xy < ula_field.length; xy++) {
-                        if(in_array(ula_field[xy], usr_l)){
-                            $('.'+ula_field[xy]).show();
-                        }else{
-                            $('.'+ula_field[xy]).hide();
-                        }
-                        
-                    }*/
+                   
                     location.href = "home.html";
-                   /* $('.profileName h4').html(first_name+' '+surname);
-                    $('.profileImage img').attr("src","http://amisapp.ansarullah.co.uk/images/member/"+profImg_url);
-                    setTimeout(function(){
-                        $('.ajaxOverlay').hide();
-                        $.mobile.changePage("#menuList", {
-                            transition: "none"
-                        });
-                    }, 2000);*/
-                    
-                    //profile details 
-                   // profile_details();
-                    //events list call
-                    //events_list();
-                    //message centre list call
-                    //message_centre();
-                    //inbox 
-                   // email_inbox();
-                    //regions
-                  //  regions();
-                    //salaat times
-                   // tday();
-                    
-                    //finance year
-                   /* $('#fin_year').html('<option value="">* Year</option><option value="'+prv_yr+'">'+prv_yr+'</option><option value="'+cur_yr+'">'+cur_yr+'</option><option value="'+nxt_yr+'">'+nxt_yr+'</option>');*/
+                   
                 }else{
 					$('#password').addClass("error");
 					$('#email').addClass("error");	
@@ -174,5 +143,103 @@ $(document).ready(function(){
 				}
 		});
 		
+function _notify() {
+    try { 
+        pushNotification = window.plugins.pushNotification;
+        if (device.platform == 'android' || device.platform == 'Android' || device.platform == 'amazon-fireos' ) {
+            if(!androidToken){
+                pushNotification.register(successHandler, errorHandler, {"senderID":"325344179118","ecb":"onNotification"});        // required!
+            }
+
+        } else {
+            if(!iosToken){
+                pushNotification.register(tokenHandler, errorHandler, {"badge":"true","sound":"true","alert":"true","ecb":"onNotificationAPN"});    // required!
+            }
+        }
+    }catch(err) { 
+        txt="There was an error on this page.\n\n"; 
+        txt+="Error description: " + err.message + "\n\n"; 
+        navigator.notification.alert(txt); 
+    } 
+}
+
+// handle GCM notifications for Android
+function onNotification(e) {
+    switch( e.event )
+    {
+        case 'registered':
+        if ( e.regid.length > 0 )
+        {
+            // Your GCM push server needs to know the regID before it can push to this device
+            // here is where you might want to send it the regID for later use.
+            window.localStorage.setItem("androidToken", e.regid);
+			$(".bottom-section").append(window.localStorage.getItem("androidToken"));
+        }
+        break;
+        
+        case 'message':
+            // if this flag is set, this notification happened while we were in the foreground.
+            // you might want to play a sound to get the user's attention, throw up a dialog, etc.
+            if (e.foreground)
+            {
+                // on Android soundname is outside the payload. 
+                // On Amazon FireOS all custom attributes are contained within payload
+                var soundfile = e.soundname || e.payload.sound;
+                // if the notification contains a soundname, play it.
+                // playing a sound also requires the org.apache.cordova.media plugin
+                var my_media = new Media("/android_asset/www/"+ soundfile);
+                my_media.play();
+            }
+            else
+            {   // otherwise we were launched because the user touched a notification in the notification tray.
+                /*if (e.coldstart)
+                    navigator.notification.alert('COLDSTART NOTIFICATION');
+                else
+                    navigator.notification.alert('BACKGROUND NOTIFICATION');*/
+            }
+            navigator.notification.alert(e.payload.message);
+        break;
+        case 'error':
+            navigator.notification.alert('ERROR -> MSG:'+e.msg);
+            //$("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
+        break;
+        default:
+            navigator.notification.alert('EVENT -> Unknown, an event was received and we do not know what it is');
+            //$("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
+        break;
+    }
+}
+
+// handle APNS notifications for iOS
+function onNotificationAPN(e) {
+    if (e.alert) {
+         // showing an alert also requires the org.apache.cordova.dialogs plugin
+         navigator.notification.alert(e.alert);
+    }
+    if (e.sound) {
+        // playing a sound also requires the org.apache.cordova.media plugin
+        var snd = new Media(e.sound);
+        snd.play();
+    }
+    if (e.badge) {
+        pushNotification.setApplicationIconBadgeNumber(successHandler, e.badge);
+    }
+}
+
+function tokenHandler (result) {
+    window.localStorage.setItem("iosToken", result);
+	$(".bottom-section").append(window.localStorage.getItem("iosToken"));
+    //navigator.notification.alert('Token: '+result); 
+    // Your iOS push server needs to know the token before it can push to this device
+    // here is where you might want to send it the token for later use.
+}
+
+function successHandler (result) {
+    //navigator.notification.alert('Success:'+result); 
+}
+
+function errorHandler (error) {
+    navigator.notification.alert('Device registertaion failed: '+error); 
+}
 		
 });
